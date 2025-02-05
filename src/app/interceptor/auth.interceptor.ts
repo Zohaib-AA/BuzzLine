3868836f7fa2e36eea37d6766ce04f6b6abf908a
Zohaib <ansarizohaibakhter@gmail.com>
2025-02-05 13:08:14 +0530

    optimization

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -1,6 +1,5 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
@@ -9,8 +8,6 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     let token = this.authService.authToken;
-    console.log(req)
-
     let authReq = req.clone({
       headers: req.headers.set('authorization', 'Bearer ' + token)
     })
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auth } from '../interfaces&constants/auth.interface';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private isAuthenticated = false;
   private token!: string;
+  private authListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route: Router) { }
 
   get authToken(): string {
-    return this.token
+    return this.token;
+  }
+
+  get userAuthentication(): boolean {
+    return this.isAuthenticated;
+  }
+
+  getAuthListener() {
+    return this.authListener.asObservable();
   }
 
   createUser(email: string, password: string, mode: string) {
@@ -28,7 +40,18 @@ export class AuthService {
       console.log(response);
       if (mode == 'login') {
         this.token = response.token;
+        if (this.token) {
+          this.isAuthenticated = true;
+          this.authListener.next(true);
+          this.route.navigate(['/']);
+        }
       }
     })
+  }
+  logout() {
+    this.isAuthenticated = false;
+    this.token = '';
+    this.authListener.next(false);
+    this.route.navigate(['/']);
   }
 }
