2f261ad8c95bee1cf4fc16eda1dfe11945fe819f
Zohaib <ansarizohaibakhter@gmail.com>
2025-02-10 16:59:48 +0530

    feat: added module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { PostListComponent } from './components/posts/post-list/post-list.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatInputModule } from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { LoginComponent } from './components/auth/login/login.component';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
 import { ErrorInterceptor } from './interceptor/error.interceptor';
 import { ErrorComponent } from './components/error/error.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -36,10 +29,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatExpansionModule, MatProgressSpinnerModule,
-    MatPaginatorModule,
     HttpClientModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   providers: [
     provideAnimationsAsync(),
